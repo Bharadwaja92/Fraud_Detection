77f4f5609617a71db6140c567a5977b2a6790832
K H SAI BHARADWAJ <saibharadwaja92@gmail.com>
2025-09-24 12:34:21 +0530

    Added the Test UI Pages

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Streamlit Test")
+st.header("Hello world from Streamlit!")
--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-uvicorn main:app --host 0.0.0.0 --port 8000 & streamlit run app.py
+#!/bin/bash
+
+# Start FastAPI in the background on port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 &
+
+# Start Streamlit in the background on port 8501
+streamlit run app.py --server.port 8501 --server.address 0.0.0.0
+
+
+# mkdir -p /home/saibharadwaj/app
+# cd /home/saibharadwaj/app
+# uvicorn main:app --host 0.0.0.0 --port 8000 & streamlit run app.py
+
