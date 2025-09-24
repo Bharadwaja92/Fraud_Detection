b236157fbd9c557334bf88d58e0e4bec0c9911dd
K H SAI BHARADWAJ <saibharadwaja92@gmail.com>
2025-09-24 12:43:09 +0530

    Updated start.sh file

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+cd /home/saibharadwaj/app/Fraud_Detection
+
 # Start FastAPI in the background on port 8000
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit in the background on port 8501
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0
 
-
 # mkdir -p /home/saibharadwaj/app
-# cd /home/saibharadwaj/app
 # uvicorn main:app --host 0.0.0.0 --port 8000 & streamlit run app.py
 
